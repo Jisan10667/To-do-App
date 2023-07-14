c7935edd9d41f54a509f39f5d870cf71b93133fb
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-07-15 00:41:02 +0600

    userExists(), findUser(), insertUser() function has been added
    Through sql commands, tryin' to manipulate data

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(
   })
 );
 
-// set view engine to ejs, allows us to use ejs in the public folder
-app.set("views", __dirname + "/public");
-app.set("view engine", "ejs");
-
 //include middleware for serving static files (css/images folder and error pages folder)
 app.use(express.static(__dirname + "/public/views"));
 app.use(express.static(__dirname + "/public/error-pages"));
+
+// set view engine to ejs, allows us to use ejs in the public folder
+app.set("views", __dirname + "/public");
+app.set("view engine", "ejs");
 
 var connectionPool = mysql.createPool({
   connectionLimit: 300,
@@ -40,6 +40,134 @@ const sessionStore = new mySQlStore(
   connectionPool
 );
 
+function register(username, password, confirmPassword) {
+  return new Promise((resolve, reject) => {
+    if (password != confirmPassword) {
+      reject(new Error("Passwords need to match."));
+    } else {
+      userExists(username)
+        .then((response) => {
+          if (response) {
+            reject(new Error(`This user already exists.`));
+          } else {
+            insertUser(username, password)
+              .then((response) => {
+                insertSettings(response.user_id)
+                  .then(() => {
+                    resolve({
+                      body: {
+                        message: `Successfully registered.`,
+                      },
+                    });
+                  })
+                  .catch((error) => {
+                    reject(new Error(error.message));
+                  });
+              })
+              .catch((error) => {
+                reject(new Error(error.message));
+              });
+          }
+        })
+        .catch((error) => {
+          reject(new Error(error.message));
+        });
+    }
+  });
+}
+
+function userExists(username) {
+  return new Promise((resolve, reject) => {
+    findUser(username)
+      .then((response) => {
+        if (response.length > 0) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch((error) => {
+        reject(new Error(error.message));
+      });
+  });
+}
+// retrieves user data by username, returns empty array if user does not exist 
+function findUser(username) {
+    return new Promise((resolve, reject) => {
+        connectionPool.getConnection((err, connection) => {
+            if (err) {
+                connection.release();
+                reject(new Error(err.message));
+            }
+            else {
+                connection.query("SELECT * FROM users WHERE USERNAME=?", username, function(error, results, fields) {
+            
+                    if (error) {
+                        reject(new Error(error.message));
+                    }
+                    else {
+                        resolve(results);
+                    }
+
+                    connection.release();
+                });
+            }
+        });
+    });
+}
+// inserts a user into the database
+function insertUser(username, password) {
+  return new Promise((resolve, reject) => {
+    genPassword(password)
+      .then((response) => {
+        const salt = response.salt;
+        const hash = response.hash;
+
+        connectionPool.getConnection((err, connection) => {
+          if (err) {
+            connection.release();
+            reject(new Error(err.message));
+          } else {
+            connection.query(
+              "INSERT INTO users (USERNAME, HASH, SALT, IS_ADMIN) VALUES (?, ?, ?, 0)",
+              [username, hash, salt],
+              function (error, results, fields) {
+                if (error) {
+                  reject(new Error(error.message));
+                } else {
+                  resolve({
+                    message: "User successfully inserted",
+                    user_id: results.insertId,
+                  });
+                }
+
+                connection.release();
+              }
+            );
+          }
+        });
+      })
+      .catch((error) => {
+        reject(new Error(error.message));
+      });
+  });
+}
+
+/************** MIDDLEWARE *********************/
+// stores session data into the database
+app.use(
+  session({
+    key: "myKey",
+    secret: "session_cookie_secret",
+    name: "mycookie",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 6000000,
+    },
+  })
+);
 app.use(passport.initialize());
 // replaces session id in request object with user data pulled from deserialize user
 app.use(passport.session());
@@ -64,6 +192,10 @@ app.get("/login", (req, res, next) => {
 // displays register page
 app.get("/register", (req, res, next) => {
   res.sendFile(__dirname + "/public/register.html");
+});
+// middleware to catch all other undefined routes, sends a 404 not found error and its error page
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/error-pages/not-found.html");
 });
 // app listens on the port
 app.listen(process.env.PORT);
